18fb9cab4281e5dc20c3db61baf149107aa5f7a3
Charlese2 <knightside@hotmail.com>
2025-01-18 19:55:03 -0700

    Add more summoner.cpp names

---
+++ include/game/summoner.h
@@ -0,0 +1,13 @@
+#include "dolphin/types.h"
+
+typedef struct {
+    u16 unk0;
+    u16 unk2;
+    u16 unk4;
+    u16 unk6;
+    u16 unk8;
+    u16 unkA;
+    u16 unkC;
+    u16 unkE;
+    u16 valid_demo;
+} mainParameters;
--- src/summoner.cpp
+++ src/summoner.cpp
@@ -1,6 +1,8 @@
+#include "game/summoner.h"
 #include "dolphin/types.h"
 #include "string.h"
 #include "game/memory.h"
+#include <stdlib.h>
 
 extern void StatusStuff(u32, u32, double, double);
 extern void SoundStuff();
@@ -10,32 +12,12 @@ extern volatile bool gHeapAlloc;
 double status1;
 double status2;
 
-const char attract_pss_string[12] = "ATTRACT.PSS";
-const char cutscene_string[9] = "cutscene";
-const char character_string[10] = "character";
-const char levelchar_string[10] = "levelchar";
-const char title_string[44] = "Summoner \xaf: A Goddess Reborn - \xaa 2002, 2003";
-const char title_credits1_string[55] = "THQ Inc. Developed by Cranky Pants Games and Volition,";
-const char title_credits2_string[52] = "Inc. Uses Bink Video. Copyright \xaa 1991-2002 by RAD";
-const char title_credits3_string[47] = "Game Tools, Inc. Summoner, Cranky Pants Games,";
-const char title_credits4_string[56] = "Volition, THQ and their respective logos are trademarks";
-const char title_credits5_string[52] = "and/or registered trademarks of THQ Inc. All rights";
-const char title_credits6_string[53] = "reserved. All other trademarks, logos and copyrights";
-const char title_credits7_string[41] = "are property of their respective owners.";
-const char title_credits8_string[10] = "LOGOS.PSS";
-const char using_sce_demo_string[28] =  "Using SCE demo parameters.\n";
-const char not_valid_demo_string[48] = "Not a valid demo.  Setting default parameters.\n";
-const char file_string[13] = "summoner.cpp";
-const char level_sequences_string[16] = "Level sequences";
-const char Cutscene_seqs_string[14] = "Cutscene seqs";
-const char fonts_pkf_string[10] = "fonts.pkf";
-const char persistent_pkf_string[15] = "persistent.pkf";
-const char s2_larget_vfn_string[13] = "s2-large.vfn";
-const char s2_small_vfn_string[13] = "s2-small.vfn";
-const char mainmenu_pkf_string[13] = "mainmenu.pkf";
-const char game_startup_time_string[36] = "**** Game startup time: %.3f ****\n";
+mainParameters parameters;
 
-short main_parameters[10];
+void Cleanup(int status) {
+    SoundStuff();
+    exit(status);
+}
 
 void Initialize() {
     Memory* memPool;
@@ -53,74 +35,48 @@ void MainLoop()
     Initialize();
 
 
-    
+
     do {
         
     } while (true);
+
+    printf("**** Game startup time: %.3f ***");
+}
+
+void setParameters() {
+    mainParameters* test;
+    bool test2;
+
+    memset(&parameters, 0xff, 18);
+    parameters.valid_demo = 0;
+    test = &parameters;
+
+    for (int i = 0; i < 18; i++) {
+        
+    }
+
+    if (parameters.valid_demo == 0) {
+        printf("Not a valid demo.  Setting default parameters.\n");
+        parameters.unk0 = 0;
+        parameters.unk2 = 1;
+        parameters.unk4 = 2;
+        parameters.unkA = 6;
+        parameters.unk6 = 0;
+        parameters.unk8 = 0;
+        parameters.unkC = 10;
+        parameters.unkE = 0;
+    } else {
+        printf("Using SCE demo parameters.\n");
+    }
+
+    test2 = parameters.unk0;
 }
 
 
-void main()
+int main()
 {
-    int remaining = (short)2;
-
-    memset(&main_parameters, 0xff, 18);
-    main_parameters[8] = (short)0;
-
-    
-    do {
-        if (main_parameters[0] != 0xff) {
-            main_parameters[8] = 1;
-            break;
-        }
-        if (main_parameters[1] != 0xff) {
-            main_parameters[8] = 1;
-            break;
-        }
-        if (main_parameters[3] != 0xff) {
-            main_parameters[8] = 1;
-            break;
-        }
-        if (main_parameters[2] != 0xff) {
-            main_parameters[8] = 1;
-            break;
-        }
-        if (main_parameters[5] != 0xff) {
-            main_parameters[8] = 1;
-            break;
-        }
-        if (main_parameters[3] != 0xff) {
-            main_parameters[8] = 1;
-            break;
-        }
-        if (main_parameters[7] != 0xff) {
-            main_parameters[8] = 1;
-            break;
-        }
-        if (main_parameters[9] != 0xff) {
-            main_parameters[8] = 1;
-            break;
-        }
-
-        remaining = remaining + -1;
-    } while (remaining != 0);
-
-
-
-    if (main_parameters[8] == 0) {
-        printf(not_valid_demo_string);
-        main_parameters[0] = 0;
-        main_parameters[1] = 1;
-        main_parameters[2] = 2;
-        main_parameters[5] = 6;
-        main_parameters[3] = 0;
-        main_parameters[4] = 6;
-        main_parameters[6] = 10;
-        main_parameters[7] = 0;
-    } else {
-        printf(using_sce_demo_string);
-    }
-
+    setParameters();
     MainLoop();
-    SoundStuff();
+    Cleanup(3);
+    return 0;
 }
