01674b9a69896e0fdc9011d24c21cd116db9aa96
Charlese2 <knightside@hotmail.com>
2025-01-23 13:56:48 -0700

    Add names for gamemem functions

--- include/game/gamemem.h
+++ include/game/gamemem.h
@@ -3,30 +3,17 @@
 extern volatile bool gHeapAlloc;
 
 class GameMem {
-    Memory memory[6];
-    char unkD8[2];
+    Memory persistantMempool;
+    Memory soundMempool;
+    Memory perlevelMempool;
+    Memory cutsceneMempool;
+    Memory spellslotMempool;
+    Memory summonMempool;
+    bool perlevel_mempool_active;
+    bool cutscene_mempool_active;
     bool summon_mempool_active;
     bool spellslot_mempool_active;
     bool persistant_mempool_active;
-    bool unkDD;
-    bool unkDE;
-    bool unkDF;
-    bool unkE0;
-    bool unkE1;
-    bool unkE2;
-    bool unkE3;
-    bool unkE4;
-    bool unkE5;
-    bool unkE6;
-    bool unkE7;
-    bool unkE8;
-    bool unkE9;
-    bool unkEA;
-    bool unkEB;
-    bool unkEC;
-    bool unkED;
-    bool unkEE;
-    bool unkEF;
 
     void activateGamemem(void);
     void allocateMempools();
